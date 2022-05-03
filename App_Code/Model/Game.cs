ad3510206ea62fd8032615f00544988ef202f651
Dearly_Puppet <dearly.artist.3d@gmail.com>
2022-05-03 19:43:51 +0200

    Agregar archivos de proyecto.

---
+++ App_Code/Model/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M05_UF3_P2_Template.App_Code.Model
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public int Product_Id { get; set; }
+        public float Rating { get; set; }
+        public string Version { get; set; }
+
+        public Game()
+        {
+
+        }
+        public Game(DataRow row)
+        {
+
+        }
+        public Game(int id) : this(DatabaseManager.Select("Game", null, "Id = " + id + " ").Rows[0]) { }
+
+        public void Update()
+        {
+
+        }
+        public void Add()
+        {
+
+        }
+    }
+}
