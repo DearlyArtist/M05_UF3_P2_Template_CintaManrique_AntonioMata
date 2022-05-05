f12b53201a367ba81965021daa29edccf2b4cc9d
antuan42 mata <antuanbalonmano42@gmail.com>
2022-05-05 17:34:18 +0200

    Creacion de la tabla video

---
+++ App_Code/Model/Video.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M05_UF3_P2_Template.App_Code.Model
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public int Product_Id { get; set; }
+        public float Duration { get; set; }
+
+        public Video()
+        {
+
+        }
+        public Video(DataRow row)
+        {
+
+        }
+        public Video(int id) : this(DatabaseManager.Select("Vídeo", null, "Id = " + id + " ").Rows[0]) { }
+
+        public void Update()
+        {
+
+        }
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+    }
+}
---
+++ App_Data/dbo.Videos.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Video] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1),
+	[Product_Id]INT NULL,
+	[Duration]FLOAT NULL,
+);
+
